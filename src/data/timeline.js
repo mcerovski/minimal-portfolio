426335818eff5d5248b27281e4edf4e50eb971d4
mcerovski <mcerovski@croz.net>
2022-12-16 16:18:08 +0100

    description

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -3,30 +3,24 @@ export default [
         year: 'Today',
         title: 'Full Stack Software Developer @ Croz',
         duration: '4 years',
-        details: 'Details :D Details :D Details :D Details :D Details :D Details :D Details :D Details :D Details :D Details :D Details :D '
+        details: 'After that much time learning and thinkering with computers and electronics came time to find ways to use those skills and make money doing same things. Couple of interviews later getting into Croz felt like home so decided to get on with coding here ever since.'
     },
     {
         year: '2013',
         title: 'Polytechnic of Zagreb - TVZ',
         duration: '5 years',
-        details: 'Details :D Details :D Details :D Details :D Details :D Details :D Details :D Details :D Details :D Details :D Details :D '
+        details: 'Finishing up highschool and still being interested in computer science even more came time to choose College. Tehničko veleučilište u Zagrebu seemed like perfect fit so i decidet to enroll and get on with acquiring real life skills in regards to computer science.'
     },
     {
         year: '2009',
         title: 'Highschool student @ I. tehnicka skola Tesla',
         duration: '4 years',
-        details: 'Details :D Details :D Details :D Details :D Details :D Details :D Details :D Details :D Details :D Details :D Details :D '
+        details: 'After showing interest in computers and electronics I decided to get education in same field'
     },
     {
         year: '2008',
         title: 'First Line of Code',
         duration: 'the beginning',
-        details: 'Details :D Details :D Details :D Details :D Details :D Details :D Details :D Details :D Details :D Details :D Details :D '
-    },
-    {
-        year: '2005',
-        title: 'Interest in computers',
-        duration: 'the beginning',
-        details: 'Details :D Details :D Details :D Details :D Details :D Details :D Details :D Details :D Details :D Details :D Details :D '
+        details: 'After building first computer and thinkering with it for couple of years I finaly acuired connection to WWW where I discovered even more possibilities to modify my machine and make it do thins i want with CMD'
     }
 ]
